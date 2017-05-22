6601125ed5b1771f118fe82d783965c7aabff879
刘晓琦 <ericliu@ikidstv.com>
2017-05-22 14:54:54 +0800

    增加代理二维码页面

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -18,7 +18,7 @@ class WelcomeController < ApplicationController
   #销售代理二维码展示
   def agent_ewm
     public_account = EricWeixin::PublicAccount.where(name: 'ugy').first
-    weixin_user = EricWeixin::WeixinUser.where(openid: params[:oid]).first
+    weixin_user = EricWeixin::WeixinUser.where(openid: params[:openid]).first
     code = ::EricWeixin::TwoDimensionCode.get_long_time_two_dimension_code app_id: public_account.weixin_app_id, scene_str: "sale-agent-#{weixin_user.openid}"
     @url = code.image_url
   end
